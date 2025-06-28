c2963f259e194f785e0c2ed45ae177d8fc10b55c
Robot K <yukai.wang@abel.ai>
2025-06-28 22:43:37 +0800

    feat

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -25,7 +25,7 @@ const HomePage: React.FC = () => {
       icon: '🤖',
     },
   ];
-  const contractAddress = "Aq1vZwY61YFGPF9cLAotxctiaCmxh1RMWXDtHRn8pump";
+  const contractAddress = "coming soon";
 
   const handleCopyAddress = () => {
     // Placeholder contract address - will be updated when token launches
